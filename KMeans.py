65df5af0bb0b99f8fa70595788f30425564df305
Akshay Syal <akshaysyal@Akshays-Laptop.local>
2024-05-31 13:46:07 -0400

    prediction function

--- KMeans.py
+++ KMeans.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 class KMeans:
-    def __init__(self,k,dist_type):
+    def __init__(self,k,dist_type,iters):
         self.k = k # number of clusters
         self.dist_type = dist_type
-    
+        self.iters = iters
+
     def distance(self,x,y):
         if(self.dist_type == 'Euclidean'):
             return np.linalg.norm(x-y)
@@ -23,14 +24,14 @@ class KMeans:
     def fit(self,data):
         self.data = data
         
-        # initializing pi (membership matrix)
-        self.pi = np.zeros((len(data),self.k), dtype=int)
-
         # initializing centroids
         self.centroids = np.random.choice(data, self.k, replace=False)
     
     def computePi(self):
         # for all data points find the closest centroid and update pi
+        # reinitializing pi everytime it gets recomputed
+        self.pi = np.zeros((len(self.data),self.k), dtype=int)
+
         for i in range(len(self.data)):
             dist = self.distance(self.data[i],self.centroids[0])
             closest_centroid_idx = 0
@@ -45,16 +46,16 @@ class KMeans:
         # for all k clusters
         # pi[i] (reshaped to 1xN) is multiplied with Xi (NxD)
         # normalized by num of data points in cluster k i.e. sum(pi[i])
-
         for k in range(self.k):
             self.centroids[k] = self.pi.T[k] @ self.data / sum(self.pi.T[k])
     
-
-    def fit(self):
-        pass
-
     def predict(self):
-        pass
+        # returns cluster lbl allocated to each data point 
+        for i in range(self.iters):
+            self.computePi()
+            self.computeCentroids()
+
+        return np.argmax(self.pi,axis=1)
 
 
     def kmeansObjective(self):
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import numpy as np
 
 if __name__ == '__main__':
-    print(np.random.choice([1,2,3], 2, replace=False))
+    X = np.array([[1,0,0,1],[1,1,0,0],[1,0,1,0]])
+    X_trans = X.T
+    X_trans[3]/2
+    A = np.array([1,0,0,1])
+    B = np.array([1,0,0,1])
+    np.dot(A,B) / (np.linalg.norm(A)*np.linalg.norm(B))
+    np.linalg.norm(B)
+
+    matrix = np.array([
+    [0, 1, 2, 0],
+    [0, 1, 0, 0],
+    [1, 0, 0, 0],
+    [0, 0, 0, 1]
+])
+
+# Get the index of the 1 in each row
+indices = np.argmax(matrix, axis=1)
+indices
